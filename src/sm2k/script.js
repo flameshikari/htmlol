97adff86d752d56a89eb217ec0df8274ce5f2d44
flameshikari <mail@hexed.pw>
2024-09-09 13:42:50 +0500

    update sm2k

--- src/sm2k/script.js
+++
@@ -1,9 +0,0 @@
-function calculateFromDate(date) {
-  date1 = new Date(date)
-  date2 = new Date()
-  diffTime = Math.abs(date2 - date1)
-  diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  return diffDays
-}
-
-document.getElementById('date').innerHTML = '<font color="f9cd3f">' + calculateFromDate('8/30/2000') + '</font>'
